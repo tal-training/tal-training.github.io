36611837d6bd6a879d9ebf4c7c066c37e5bfa6ba
Training Repositories <33219767+tal-training@users.noreply.github.com>
2024-02-08 21:32:58 +0200

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,3 +1,3 @@
 import 'https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.7/axios.min.js'
 
-axios.get('test.json').then(response=>document.body.innerHTML=response.data)
+axios.get('test.json').then(response=>document.body.innerHTML=JSON.parse(response.data).name)
