c4324120c5edf7d49b8e87e8a39954c0a8b31053
Training Repositories <33219767+tal-training@users.noreply.github.com>
2024-02-14 12:43:52 +0200

    ping search

--- search.js
+++ search.js
@@ -2,7 +2,7 @@ import 'https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.7/axios.min.js'
 
 axios.get('big-json.json').then(response=>showSearch(response.data))
 
-let SERVER_ADDRESS="https://tmanor.pythonanywhere.com/ping?section"
+let SERVER_ADDRESS="http://tmanor.pythonanywhere.com/ping?section"
 
 function ping(section){
     axios.get(`${SERVER_ADDRESS}=${section}`)
@@ -12,8 +12,10 @@ function showSearch(data){
     // let sections=data.map(i=>{filename:i.filename.split('-')[0].split('.')[0]})
     let s=document.getElementById("search")
     s.oninput=(e)=>{
-        Array.from(document.getElementsByClassName("item")).map(i=>i.children[0].innerHTML.toLowerCase().includes(e.target.value.toLowerCase())||i.children[1].innerHTML.toLowerCase().includes(e.target.value.toLowerCase())?i.children`[0].style.color="orange":i.children[0].style.color="white");
-        ping(e.target.value)
+        Array.from(document.getElementsByClassName("item")).map(i=>i.children[0].innerHTML.toLowerCase().includes(e.target.value.toLowerCase())||i.children[1].innerHTML.toLowerCase().includes(e.target.value.toLowerCase())?i.children[0].style.color="orange":i.children[0].style.color="white")
+    }
+    s.onchange=(e)=>{
+        ping(e.target.value)        
     }
     
     document.getElementById("sections").innerHTML=data.map(i=>`<div class="item"><div class="title" >${i.filename.replace("-images", "")}</div><div class="content" >
